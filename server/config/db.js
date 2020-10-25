5278b371993553969eaaf1d4f00eca57685da513
FarzamMohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2020-10-25 17:00:52 -0400

    Update. MongoDB connected to Atlas
    Changed local database to online/global database.

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,8 @@
+/*
 module.exports = 
 {
     "URI": "mongodb://localhost/contacts_list"
 }
+*/
+
+module.exports.URI = "mongodb+srv://farzam:aQK2oQrtmZedssv3@mongodbserver.htwl4.mongodb.net/comp229_assignment?retryWrites=true&w=majority"
